233e9da8dd68cb794e6120582a66135ae6c60a15
Asa Gilmore <asaagilmore@gmail.com>
2024-09-23 14:09:37 -0700

    h100 support maybe

--- dinoDocker/entrypoint.sh
+++ dinoDocker/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
-pip install --no-cache-dir --extra-index-url https://pypi.nvidia.com cuml-cu11
+pip install --extra-index-url https://pypi.nvidia.com cuml-cu12
+
+pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu118
+
+pip install /app/dinov2
 
 exec "$@"
